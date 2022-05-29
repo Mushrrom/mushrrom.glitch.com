eb96eaa453ab0d3ccd2c3a9efdad85d7160b557a
Glitch (glitch-hello-website) <none>
2022-05-29 23:21:55 +0000

    📮🍓 Checkpoint ./script.js:39241923/152

--- script.js
+++ script.js
@@ -36,3 +36,9 @@ $.terminal.defaults.formatters.push(function (string) {
     })
     .join("");
 });
+
+function myFunction(p1, p2) {
+  return p1 * p2;   // The function returns the product of p1 and p2
+};
+
+console.log(myFunction(5, 6));
